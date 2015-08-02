fb5c1f5b1d4c4b4f0f70294d6883d8a02f8eda0b
Alexey Chulochnikov <chulochnikov@gmail.com>
2015-08-02 12:25:45 +0300

    - setup project

---
+++ github client/github client/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  github client
+//
+//  Created by Alexey Chulochnikov on 02.08.15.
+//  Copyright (c) 2015 Alexey Chulochnikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

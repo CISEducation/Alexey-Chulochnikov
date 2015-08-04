5c1ef6962693a87fc5fcf483b4961af1e8eec30f
Alexey Chulochnikov <chulochnikov@gmail.com>
2015-08-04 23:03:13 +0300

    - login view controller added to the project

---
+++ github client/github client/Sources/Presentation/Controllers/Login/GHLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  GHLoginViewController.h
+//  github client
+//
+//  Created by Alexey Chulochnikov on 04.08.15.
+//  Copyright © 2015 Alexey Chulochnikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GHLoginViewController : UIViewController
+
+@end
